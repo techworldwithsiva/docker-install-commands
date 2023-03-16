f98128b046f4adee69e50c7d8e203ccf8b0102ba
siva <siva@ivs.tv>
2023-03-16 07:55:41 +0530

    some msg

--- docker-install.sh
+++ docker-install.sh
@@ -10,3 +10,31 @@ if [ $USER_ID -ne 0 ]; then
 	echo  -e "$R You are not the root user, you dont have permissions to run this $N"
 	exit 1
 fi
+
+VALIDATE(){
+	if [ $1 -ne 0 ]; then
+		echo -e "$2 ... $R FAILED $N"
+		exit 1
+	else
+		echo -e "$2 ... $G SUCCESS $N"
+	fi
+
+}
+
+yum update  -y
+VALIDATE $? "Updating packages"
+
+amazon-linux-extras install docker -y
+VALIDATE $? "Installing Docker"
+
+service docker start
+VALIDATE $? "Starting Docker"
+
+systemctl enable docker
+VALIDATE $? "Enabling Docker"
+
+usermod -a -G docker ec2-user
+VALIDATE $? "Added ec2-user to docker group"
+
+echo  -e "$R You need logout and login to the server $N"
+
